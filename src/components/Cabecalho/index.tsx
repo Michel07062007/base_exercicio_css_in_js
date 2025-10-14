0bd5c0c2e0d97d96ee59041fe6b5ea85f04d5e86
Hael <michelkleinth@gmail.com>
2025-10-13 23:49:59 -0300

    initial commit

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,16 @@
-import styles from './Cabecalho.module.css'
+import React from "react";
+import styled from "styled-components";
 
-const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
-)
 
-export default Cabecalho
+const Cabecalho = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`;
+
+export function Header() {
+  return (
+    Cabecalho
+  );
+}
